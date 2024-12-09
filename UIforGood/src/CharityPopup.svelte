e2daa69db6ea52369539c7bac3c2c6d9ee90114d
24varshag <24varshag@gmail.com>
2024-12-08 23:58:50 -0500

    eye.png

--- UIforGood/src/CharityPopup.svelte
+++ UIforGood/src/CharityPopup.svelte
@@ -181,7 +181,7 @@ h4 {
 
 #show_bg_2 {
     background-image: linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.856)),
-        url("public/eye.png");
+        url("../public/eye.png");
     width: calc(100% - 20px);
     height: 300px; 
     background-size: cover;
